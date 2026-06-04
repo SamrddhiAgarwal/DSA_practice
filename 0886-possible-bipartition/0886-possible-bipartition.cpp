04fe511f1e280cb93f648f554d699e338c697293
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-06-05 04:22:49 +0530

    Time: 71 ms (9.38%), Space: 76.3 MB (16.06%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    bool dfs(vector<vector<int>>& adj,vector<int>& color,int v)
+    {
+        for(auto u:adj[v])
+        {
+            if(color[u]==0)
+            {
+                color[u]=-color[v];
+                if(dfs(adj,color,u)==false) return false;
+            }
+            else
+            {
+                if(color[u]==color[v])
+                return false;
+            }
+        }
+        return true;
+    }
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         
         vector<vector<int>> graph(n+1);
@@ -12,7 +29,20 @@ public:
         }
 
         vector<int> color(n+1,0);
-        queue<int>q;
+
+        for(int i=1;i<=n;i++)
+        {
+            if(color[i]==0)
+            {
+                color[i]=1;
+                if(dfs(graph,color,i)==false)
+                return false;
+            }
+        }
+        return true;
+
+
+        /*queue<int>q;
 
         for(int i=1;i<=n;i++)
         {
@@ -42,6 +72,6 @@ public:
             
         }
         return true;
-    
+        */
     }
 };
