cfe2baa1b5a64cd0018b1d6afc57789d4084ab20
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-06-05 04:07:18 +0530

    Time: 70 ms (10.39%), Space: 76.3 MB (16.06%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        
+        vector<vector<int>> graph(n+1);
+        for(auto E: dislikes)
+        {
+            int u=E[0];
+            int v=E[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+
+        vector<int> color(n+1,0);
+        queue<int>q;
+
+        for(int i=1;i<n;i++)
+        {
+            if(color[i]==0)
+            {
+                color[i]=1;
+                q.push(i);
+                while(!q.empty())
+                {
+                    int v=q.front();
+                    q.pop();
+                    for(auto u:graph[v])
+                    {
+                        if(color[u]==0)
+                        {
+                            color[u]=-color[v];                    
+                            q.push(u);
+                        }
+                        else
+                        {
+                            if(color[u]==color[v])
+                            return false;
+                        }                
+                    }
+                }
+            }
+            
+        }
+        return true;
+    
+    }
+};
