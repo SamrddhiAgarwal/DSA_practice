e6a3420b1e719b0d76383ff4e00241a2af5fae56
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-06-04 23:17:31 +0530

    Time: 10 ms (86.38%), Space: 34.7 MB (74.9%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+vector<vector<int>> bfs(vector<vector<int>>& grid)
+    {
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<int>> ans(n,vector<int>(m,-1));
+        queue<pair<int,int>> q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==0)
+                q.push({i,j});
+                // ans[i][j]=0;
+            }
+        }
+        int time=0;
+        int dx[]={0,1,0,-1};
+        int dy[]={-1,0,1,0};
+        while(!q.empty())
+        {
+            
+            int sz=q.size();
+            while(sz>0)
+            {
+                auto [r,c]=q.front();
+                q.pop();
+                ans[r][c]=time;
+                // int dx[]={0,1,0,-1};
+                // int dy[]={-1,0,1,0};
+                for(int i=0;i<4;i++)
+                {
+                    int x=r+dx[i];
+                    int y=c+dy[i];
+                    if(x>=0 && x<n && y>=0 && y<m && grid[x][y]==1 && ans[x][y]==-1)
+                    {
+                        grid[x][y]=0;
+                        q.push({x,y});
+                        
+                    }
+                    
+                }
+                sz--;
+            }
+            time++;
+        }
+        return ans;
+    }
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        return bfs(mat);
+    }
+};
