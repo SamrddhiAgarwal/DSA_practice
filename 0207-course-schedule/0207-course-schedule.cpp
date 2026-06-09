f141a163d3acbe5caa65f399a420ffc3c2314e4d
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-06-09 22:29:15 +0530

    Time: 0 ms (100%), Space: 18.9 MB (93.33%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,34 +1,74 @@
+// class Solution {
+// public:
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         vector<int> adj[numCourses];
+//         vector<int> indegree(numCourses,0);
+//         for(auto& p:prerequisites)
+//         {
+//             adj[p[1]].push_back(p[0]);
+//             indegree[p[0]]++;
+//         }
+//         queue<int>q;
+//         for(int i=0;i<numCourses;i++)
+//         {
+//             if(indegree[i]==0)
+//             q.push(i);
+//         }
+//         int cnt=0;
+//         while(!q.empty())
+//         {
+//             int b=q.front();
+//             q.pop();
+//             cnt++;
+//             for(auto &a:adj[b])
+//             {
+//                 indegree[a]--;
+
+//                 if(indegree[a]==0)
+//                 q.push(a);
+//             }
+//         }
+//         if(cnt==numCourses) return true;
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
+    bool dfs(int v, vector<int>& vis, vector<int> adj[], vector<int>& pvis)
+    {
+        vis[v]=1;
+        pvis[v]=1;
+
+        for(auto& u:adj[v])
+        {
+            if(!vis[u])
+            {
+                if(dfs(u,vis,adj,pvis)) return true;
+            }
+            
+            else if(pvis[u])
+            return true;
+        }
+        pvis[v]=0;
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> adj[numCourses];
-        vector<int> indegree(numCourses,0);
         for(auto& p:prerequisites)
         {
             adj[p[1]].push_back(p[0]);
-            indegree[p[0]]++;
         }
-        queue<int>q;
+        vector<int>vis(numCourses,0);
+        vector<int>pvis(numCourses,0);
         for(int i=0;i<numCourses;i++)
         {
-            if(indegree[i]==0)
-            q.push(i);
-        }
-        int cnt=0;
-        while(!q.empty())
-        {
-            int b=q.front();
-            q.pop();
-            cnt++;
-            for(auto &a:adj[b])
+            if(!vis[i])
             {
-                indegree[a]--;
-
-                if(indegree[a]==0)
-                q.push(a);
+                if(dfs(i,vis,adj,pvis))
+                return false;
             }
         }
-        if(cnt==numCourses) return true;
-        return false;
+        return true;
     }
 };
