2e4be23e862a40d8ac19a48413848a8b3069236b
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-06-12 03:28:54 +0530

    Time: 51 ms (52.39%), Space: 70.9 MB (44.07%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -14,27 +14,34 @@ public:
         }
         vector<double>dist(n,0.0);
         
-        priority_queue<p>pq; 
-        pq.push({1.0,{start_node,-1}});
+        // priority_queue<p>pq; 
+        priority_queue<pair<double,int>> pq;
+        // pq.push({1.0,{start_node,-1}});
+        pq.push({1.0,start_node});
         dist[start_node]=1.0;
-        bool found =false;
+        // bool found =false;
         while(!pq.empty())
         {
             double d=pq.top().first;
-            auto [v,parent]=pq.top().second;
+            // auto [v,parent]=pq.top().second;
+            //no parent needed since pq will already give bigger one and we will go for first value of end so that it don;t form cycle
+            int v=pq.top().second;
             pq.pop();
-            if(v==end_node) found=true;
+            // if(v==end_node) found=true;
+            if(v==end_node) return d;
             if(dist[v]>d) continue;
             for(auto& [u,w]:adj[v])
             {
-                if(u==parent) continue;
+                // if(u==parent) continue;
                 if(dist[u]<w*d)
                 {
                     dist[u]=w*d;
-                    pq.push({dist[u],{u,v}});
+                    // pq.push({dist[u],{u,v}});
+                    pq.push({dist[u],u});
                 }
             }
         }
-        return (found)?dist[end_node]:0.00;
+        return dist[end_node];
+        // return (found)?dist[end_node]:0.00; not needed was doing for 1 but it will not come since 1 is probability is possible only 1 is there 
     }
 };
