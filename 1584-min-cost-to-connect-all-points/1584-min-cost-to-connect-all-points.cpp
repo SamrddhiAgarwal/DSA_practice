e4c4889948fe2b359a184c6541982167b29c35df
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-06-14 19:32:32 +0530

    Time: 10 ms (99.52%), Space: 14.6 MB (86.96%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,56 +1,70 @@
 class Solution {
 public:
-typedef pair<int,pair<int,int>> p;
     int minCostConnectPoints(vector<vector<int>>& points) {
 
-    //     long long ans=0;int n=points.size();
-    //     for(int i=0;i<n-1;i++)
-    //     {
-    //         int mn=INT_MAX;
-    //         for(int j=0;j<n;j++)
-    //         {
-    //             if(i==j) continue;
-                
-    //             int val= abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
-    //             if(mn<val)
-    //             {
-    //                 mn=val;
-                    
-    //             }
-    //             ans+=mn;
-    //         }
-    //     }
-    //     return ans;
-        int n=points.size();
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        int ans=0;int n=points.size();
+        vector<int> dist(n,INT_MAX);
         vector<int> vis(n,0);
-        vector<int>dist(n,INT_MAX);
-        pq.push({0,0});
         dist[0]=0;
-        int ans=0;
-        while(!pq.empty())
+        vis[0]=1;
+        int curr=0;  // imp since we need to to find new value as per new min not i 
+        for(int i=0;i<n-1;i++)
         {
-            auto [val,i]=pq.top();
-            pq.pop();
-            if(!vis[i])
+            int mn=INT_MAX;int x2=0;
+            
+            for(int j=0;j<n;j++)
             {
-                vis[i]=1;
-                ans+=val;   
-                int mn=INT_MAX;int x2;
-                for(int j=0;j<n;j++)
+                if(vis[j]==1) continue;
+                int val= abs(points[curr][0]-points[j][0])+abs(points[curr][1]-points[j][1]);
+                
+                if(dist[j]>val)
                 {
-                    if(vis[j]==1) continue;
-                    
-                    int val2= abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
-                    if(dist[j]>val2)
-                    {
-                        pq.push({val2,j});
-                        dist[j]=val2;
-                    }
-                    
-                }                
+                    dist[j]=val;
+                }
+                if(mn>dist[j])
+                {
+                    mn=dist[j];
+                    x2=j;
+                }
             }
+            
+            ans+=mn;
+            vis[x2]=1;
+            curr=x2;
+            
+            
         }
         return ans;
+        // int n=points.size();
+        // priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        // vector<int> vis(n,0);
+        // vector<int>dist(n,INT_MAX);
+        // pq.push({0,0});
+        // dist[0]=0;
+        // int ans=0;
+        // while(!pq.empty())
+        // {
+        //     auto [val,i]=pq.top();
+        //     pq.pop();
+        //     if(!vis[i])
+        //     {
+        //         vis[i]=1;
+        //         ans+=val;   
+        //         int mn=INT_MAX;int x2;
+        //         for(int j=0;j<n;j++)
+        //         {
+        //             if(vis[j]==1) continue;
+                    
+        //             int val2= abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+        //             if(dist[j]>val2)
+        //             {
+        //                 pq.push({val2,j});
+        //                 dist[j]=val2;
+        //             }
+                    
+        //         }                
+        //     }
+        // }
+        // return ans;
     }
 };
