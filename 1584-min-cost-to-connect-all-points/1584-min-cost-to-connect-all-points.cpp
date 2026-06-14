1a4740477e91b70ca71f9516d8c0dfcd935ed1a4
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-06-14 18:35:09 +0530

    Time: 378 ms (43.02%), Space: 46.6 MB (58.44%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+typedef pair<int,pair<int,int>> p;
+    int minCostConnectPoints(vector<vector<int>>& points) {
+
+    //     long long ans=0;int n=points.size();
+    //     for(int i=0;i<n-1;i++)
+    //     {
+    //         int mn=INT_MAX;
+    //         for(int j=0;j<n;j++)
+    //         {
+    //             if(i==j) continue;
+                
+    //             int val= abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+    //             if(mn<val)
+    //             {
+    //                 mn=val;
+                    
+    //             }
+    //             ans+=mn;
+    //         }
+    //     }
+    //     return ans;
+        int n=points.size();
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<int> vis(n,0);
+        pq.push({0,0});
+        int ans=0;
+        while(!pq.empty())
+        {
+            auto [val,i]=pq.top();
+            pq.pop();
+            if(!vis[i])
+            {
+                vis[i]=1;
+                ans+=val;   
+                int mn=INT_MAX;int x2;
+                for(int j=0;j<n;j++)
+                {
+                    if(vis[j]==1) continue;
+                    
+                    int val2= abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+                    pq.push({val2,j});
+                }                
+            }
+        }
+        return ans;
+    }
+};
