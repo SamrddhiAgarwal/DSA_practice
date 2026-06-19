c021eb6f3967d5350aaa730e7486a556f2392331
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-06-19 15:13:36 +0530

    Time: 3 ms (38%), Space: 41.1 MB (42.03%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,15 +4,38 @@ public:
         int n=nums.size();
         vector<int> v(n,1);
         vector<int> right(n,1);
-        
+        int pro,cnt;
+        if(nums[0]==0)
+        {
+            pro=1;
+            cnt=1;
+        }
+        else
+        {
+            pro=nums[0];
+            cnt=0;
+        }
         for(int i=1;i<n;i++)
         {
             v[i]=v[i-1]*nums[i-1];
+            if(nums[i]==0)
+            {
+                cnt++;
+                continue;
+            }
+            pro=pro*nums[i];
         }
         for(int i=n-2;i>=0;i--)
         {
-            right[i]=right[i+1]*nums[i+1];
-            v[i]=v[i]*right[i];
+            if(nums[i]!=0)
+            v[i]=v[i+1]*nums[i+1]/nums[i];
+            else
+            {
+                if(cnt<=1)
+                v[i]=pro;
+                else
+                v[i]=0;
+            }
         }
         return v;
     }
