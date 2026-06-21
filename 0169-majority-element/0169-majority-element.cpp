c9920b69be59db882ae7d49b27aeeaa1f9e2c089
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-06-21 19:55:23 +0530

    Time: 5 ms (12.58%), Space: 28.3 MB (27.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,32 +1,47 @@
 class Solution {
 public:
+    // int majorityElement(vector<int>& nums) {
+    //     int cnt=0,a;
+    //     for(auto& n:nums)
+    //     {
+    //         if(cnt==0)
+    //         {
+    //             a=n;
+    //             cnt=1;
+    //         }
+    //         else if(n==a)
+    //         {
+    //             cnt++;
+    //         }
+    //         else
+    //         {
+    //             cnt--;
+    //         }
+    //     }
+    //     int cnt2=0;
+    //     for(auto& n:nums)
+    //     {
+    //         if(a==n)
+    //         cnt2++;
+    //     }
+    //     if(cnt2 > nums.size()/2)
+    //     return a;
+    //     return -1;
+        
+    // }
     int majorityElement(vector<int>& nums) {
-        int cnt=0,a;
-        for(auto& n:nums)
+        int len=32,ans=0;;
+        for(int i=0;i<len;i++)
         {
-            if(cnt==0)
+            int cnt=0;
+            for(auto n:nums)
             {
-                a=n;
-                cnt=1;
-            }
-            else if(n==a)
-            {
+                if(n & (1<<i))
                 cnt++;
             }
-            else
-            {
-                cnt--;
-            }
+            if(cnt>nums.size()/2)
+            ans+=(1<<i);
         }
-        int cnt2=0;
-        for(auto& n:nums)
-        {
-            if(a==n)
-            cnt2++;
-        }
-        if(cnt2 > nums.size()/2)
-        return a;
-        return -1;
-        
+        return ans;
     }
 };
