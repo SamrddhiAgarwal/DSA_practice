1abf4cbe98369e280bdbedf486ad599cc49e4c16
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-06-26 15:09:12 +0530

    Time: 0 ms (100%), Space: 12.1 MB (34.91%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        unordered_set<int> st;
+        for(auto n:arr)
+        {
+            mp[n]++;
+        }
+        for(auto it:mp)
+        {
+            if(st.find(it.second)!=st.end())
+            return false;
+            st.insert(it.second);
+        }
+        return true;
+    }
+};
