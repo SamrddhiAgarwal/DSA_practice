36e3886dcd5a6a69e18d1c78474feaa8e57aef62
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-07-04 21:57:36 +0530

    Time: 23 ms (5.64%), Space: 67.2 MB (5.17%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int a=0;
+        string ns="";
+        for(auto c:s)
+        {
+            if(c=='(')
+            {
+                a++;
+                if(a==1)
+                continue;
+            }
+            if(c==')')
+            {
+                a--;
+                if(a==0)
+                continue;
+            }
+            ns=ns+c;
+        }
+        return ns;
+    }
+};
