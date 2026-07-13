70da244ab4d11652065cc418c8b99cbe7d88ecf6
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-07-13 12:47:08 +0530

    Time: 14 ms (90.42%), Space: 107 MB (87.84%) - LeetHub

---
+++ 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        int n=nums.size();
+        int mx=*(max_element(nums.begin(),nums.end()));
+        vector<int> cumFreq(mx+k+1,0);
+        for(int i=0;i<n;i++)
+        {
+            cumFreq[nums[i]]++;
+        }
+
+        for(int i=1;i<mx+k+1;i++)
+        {
+            cumFreq[i]=cumFreq[i]+cumFreq[i-1];
+        }
+
+        int ans=0;
+        for(int i=0;i<mx+1;i++)
+        {
+            if(cumFreq[i]==0) continue;
+            int possFreq,freq;
+            if (i-1>=0)
+            freq=cumFreq[i]-cumFreq[i-1];
+            else 
+            freq=cumFreq[i];
+            // if(i-k-1>=0 && i+k<mx+k+1)
+            if(i-k-1>=0)
+            possFreq=cumFreq[i+k]-cumFreq[i-k-1];
+            // else if(i+k<mx+k+1)
+            else
+            possFreq=cumFreq[i+k];
+
+            int maxOperations=min(possFreq-freq,numOperations);
+            ans=max(ans,maxOperations+freq);
+        }
+        return ans;
+    }
+};
