34f51f0d3db472412703e18e98e47e046d3fd766
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-07-16 17:27:04 +0530

    Time: 43 ms (90.77%), Space: 14.6 MB (90.28%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-    int findLength(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size(),n2=nums2.size();
-        unordered_map<int,vector<int>>mp;
-        for(int i=0;i<n2;i++)
-        {
-            mp[nums2[i]].push_back({i});
-        }
-        int mx=0;
-        for(int i=0;i<n1;i++)
-        {
-            if(mp.find(nums1[i])!=mp.end())
-            {
-                for(int j=0;j<mp[nums1[i]].size();j++)
-                {
-                    int l1=mp[nums1[i]][j], l2=i; 
-                    if(!(l1+mx<n2 && l2+mx <n1 && nums2[l1+mx]==nums1[l2+mx])) continue;
-                    while(l1<n2 && l2<n1 && nums2[l1]==nums1[l2])
-                    {
-                        l1++;
-                        l2++;
-                    }
+    int findLength(vector<int>& str1, vector<int>& str2) {
+           int n = str1.size();
+        int m = str2.size();
+    
+        vector<int> prev(m+1, 0);
+        vector<int> cur(m+1, 0);  
+        
+        int ans = 0; 
+    
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                // Characters match, increment substring length
+                if(str1[i-1] == str2[j-1]){
+                    int val = 1 + prev[j-1]; 
+                    cur[j] = val; 
                     
-                    mx=max(mx,l2-i);
-
+                    /* Update the maximum substring
+                    length found so far*/
+                    ans = max(ans, val); 
                 }
+                else{
+                    /* Characters don't match,
+                    substring length becomes 0*/
+                    cur[j] = 0; 
+                }
+                
             }
+            /* Update the previous row with 
+            the values of the current row*/
+            prev = cur; 
         }
-        return mx;
+        // Return the length of Longest Common Substring
+        return ans;
     }
 };
