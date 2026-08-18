e47e9ab49ad2be082f350d50c5926c68e3abf1e5
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-08-19 02:33:34 +0530

    Time: 4 ms (77.93%), Space: 14 MB (65.17%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -2,30 +2,21 @@ class Solution {
 public:
     string removeDuplicates(string s, int k) {
         
-        vector<pair<char,int>>st;
-
-        for(auto& c:s)
+        int n=s.size();
+        vector<int>count(n,0);
+        int j=0;
+        for(int i=0;i<n;i++)
         {
-            if(!st.empty() && c==st.back().first)
-            st.back().second++;
-
-            else
-            st.push_back({c,1});
-
-            if(st.back().second==k)
-            st.pop_back();
+            s[j]=s[i];
+            count[j]= (j>0 && s[j]==s[j-1])? count[j-1]+1:1;
+            if(count[j]==k)
+            j-=k;
+            // else 
+            j++;
         }
-        string ns="";
-        for(auto& [c,cnt]:st)
-        {
-            ns.append(cnt,c);
-        }
-        // reverse(ns.begin(),ns.end());
-        return ns;
+        return s.substr(0,j);
     }
 };
-
-
 
 
 
@@ -33,40 +24,71 @@ public:
 // public:
 //     string removeDuplicates(string s, int k) {
         
-//         string temp="",ns="";
-//         int cnt=1;
-//         while(true)
+//         vector<pair<char,int>>st;
+
+//         for(auto& c:s)
 //         {
-//             temp+=s[0];
-//             cnt=1;
-//             for(int i=1;i<s.size();i++)
-//             {
-                
-//                 if(s[i]==s[i-1])
-//                 {
-//                     cnt++;
-//                 }
-//                 else
-//                 {
-//                     cnt=1;
-//                     ns+=temp;
-//                     temp="";
-//                 }
-//                 temp+=s[i];
-//                 if(cnt==k)
-//                 {
-//                     temp="";
-//                     cnt=0;
-//                 }
-//             }
-//             ns+=temp;
-//             // return ns;
-//             if(ns==s) return ns;
-//             s=ns;
-//             // return s;
-//             ns="";
-//             // return ns;
+//             if(!st.empty() && c==st.back().first)
+//             st.back().second++;
+
+//             else
+//             st.push_back({c,1});
+
+//             if(st.back().second==k)
+//             st.pop_back();
 //         }
-//         return s;
+//         string ns="";
+//         for(auto& [c,cnt]:st)
+//         {
+//             ns.append(cnt,c);
+//         }
+//         // reverse(ns.begin(),ns.end());
+//         return ns;
 //     }
 // };
+
+
+
+
+
+// // class Solution {
+// // public:
+// //     string removeDuplicates(string s, int k) {
+        
+// //         string temp="",ns="";
+// //         int cnt=1;
+// //         while(true)
+// //         {
+// //             temp+=s[0];
+// //             cnt=1;
+// //             for(int i=1;i<s.size();i++)
+// //             {
+                
+// //                 if(s[i]==s[i-1])
+// //                 {
+// //                     cnt++;
+// //                 }
+// //                 else
+// //                 {
+// //                     cnt=1;
+// //                     ns+=temp;
+// //                     temp="";
+// //                 }
+// //                 temp+=s[i];
+// //                 if(cnt==k)
+// //                 {
+// //                     temp="";
+// //                     cnt=0;
+// //                 }
+// //             }
+// //             ns+=temp;
+// //             // return ns;
+// //             if(ns==s) return ns;
+// //             s=ns;
+// //             // return s;
+// //             ns="";
+// //             // return ns;
+// //         }
+// //         return s;
+// //     }
+// // };
