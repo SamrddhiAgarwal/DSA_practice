68860ea98c631c7e21b47cf1d17ce81f2379f837
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-08-23 12:12:50 +0530

    Time: 2 ms (71.75%), Space: 65.1 MB (41.8%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -4,13 +4,36 @@ public:
         //sorting
         //hashing 
         //with modyifying
-        for(auto &n:nums)
+                // for(auto &n:nums)
+                // {
+                //     int x=abs(n); //if we go by x=abs(x) we are colllecting refrences
+                //     if(nums[x-1]<0)
+                //     return x;
+                //     nums[x-1]=-nums[x-1];
+                // }
+                // return -1;
+
+        // need implemetation hare and tortoise
+        // int slow=nums[0],fast=nums[nums[0]];
+        // while(slow!=fast)
+        // {
+        //     slow=nums[slow];
+        //     fast=nums[nums[fast]];
+        // }
+        int slow=0,fast=0;
+        do
         {
-            int x=abs(n); //if we go by x=abs(x) we are colllecting refrences
-            if(nums[x-1]<0)
-            return x;
-            nums[x-1]=-nums[x-1];
+            slow=nums[slow];
+            fast=nums[nums[fast]];
         }
-        return -1;
+        while(slow!=fast);
+        // fast=0;
+        int slow2=0;
+        while(slow!=slow2)
+        {
+            slow=nums[slow];
+            slow2=nums[slow2];
+        }
+        return slow;
     }
 };
