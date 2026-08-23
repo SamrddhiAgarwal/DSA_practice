8d1cc54a750caeb7dbf070d35e1a30e375538d32
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-08-23 11:11:45 +0530

    Time: 2 ms (91.81%), Space: 84.2 MB (91.81%) - LeetHub

--- 1562-find-latest-group-of-size-m/1562-find-latest-group-of-size-m.cpp
+++ 1562-find-latest-group-of-size-m/1562-find-latest-group-of-size-m.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int findLatestStep(vector<int>& arr, int m) {
         int n=arr.size();
-        vector<int>len(n+2,0),cnt(n+1,0);
-        int ans=-1;
+        // vector<int>len(n+2,0),cnt(n+1,0);
+        vector<int>len(n+2,0);
+        int ans=-1,cnt_m=0;
         for(int i=0;i<n;i++)
         {
             int x=arr[i];
@@ -13,15 +14,19 @@ public:
 
             int newlen=right+1+left;
 
-            cnt[right]--;
-            cnt[left]--;
-            cnt[newlen]++;
+            // cnt[right]--;
+            if(right==m) cnt_m--;
+            // cnt[left]--;
+            if(left==m) cnt_m--;
+            // cnt[newlen]++;
+            if(newlen==m) cnt_m++;
 
             len[x]=newlen;
             len[x+right]=newlen;
             len[x-left]=newlen;
 
-            if(cnt[m]>0)
+            // if(cnt[m]>0)
+            if(cnt_m>0)
             ans=i+1;
 
         }
