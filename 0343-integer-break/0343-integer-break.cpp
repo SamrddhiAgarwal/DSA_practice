439b5bb770592c5fc0a4b58b73386d5fff5e67d8
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-08-24 12:09:24 +0530

    Time: 0 ms (100%), Space: 7.8 MB (92.57%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        vector<int>dp(max(n+1,7),0);
-        dp[1]=1;
-        dp[2]=1;
-        dp[3]=2;
-        dp[4]=4;
-        dp[5]=6;
-        dp[6]=9;
+        // vector<int>dp(max(n+1,7),0);
+        // dp[1]=1;
+        // dp[2]=1;
+        if(n==2) return 1;
+        // dp[3]=2;
+        if(n==3) return 2;
+        // dp[4]=4;
+        int third=4;
+        if(n==4) return 4;
+        // dp[5]=6;
+        int second=6;
+        if(n==5) return 6;
+        // dp[6]=9;
+        int first=9;
+        if(n==6) return 9;
         for(int i=7;i<=n;i++)
         {
-            dp[i]=3*dp[i-3];
+            //dp[i]=3*dp[i-3];
+            int ans=third*3;
+            third=second;
+            second=first;
+            first=ans;
         }
-        return dp[n];
+        return first;
     }
 };
