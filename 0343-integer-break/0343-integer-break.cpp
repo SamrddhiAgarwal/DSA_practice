2d71aa908a914b53837c17cb8dce8bdf5f3b25cc
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-08-24 11:43:00 +0530

    Time: 1 ms (15%), Space: 8.5 MB (54.92%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int>dp(max(n+1,7),0);
+        dp[1]=1;
+        dp[2]=1;
+        dp[3]=2;
+        dp[4]=4;
+        dp[5]=6;
+        dp[6]=9;
+        for(int i=7;i<=n;i++)
+        {
+            dp[i]=3*dp[i-3];
+        }
+        return dp[n];
+    }
+};
