b26721f0e5b918a382299933e21a8cb9f51c3b82
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-09-07 00:18:59 +0530

    Time: 684 ms (74.33%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT s.user_id,
+    ROUND(
+        COALESCE(SUM(c.action='confirmed')/COUNT(*),0)
+        ,2) 
+    AS confirmation_rate
+FROM Signups s LEFT JOIN Confirmations c
+ON s.user_id=c.user_id
+GROUP BY s.user_id
+-- sum is used instaed of count since = will give 1 and 0 values leadig to count of zeroes also 
+-- coalesce is used as coalesce(null,val1,val2,null) returns first val which is not null as here val1 
