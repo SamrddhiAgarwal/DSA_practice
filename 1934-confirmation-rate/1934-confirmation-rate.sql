3144fa01cdb655550cb013fe154a204dd6360292
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-09-07 00:23:47 +0530

    Time: 644 ms (88.89%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -9,3 +9,16 @@ ON s.user_id=c.user_id
 GROUP BY s.user_id
 -- sum is used instaed of count since = will give 1 and 0 values leadig to count of zeroes also 
 -- coalesce is used as coalesce(null,val1,val2,null) returns first val which is not null as here val1 
+-- method-2 
+    -- IFNULL(
+    --     ROUND(
+    --             COUNT(CASE WHEN 
+    --                 c.action = 'confirmed' THEN 1 
+    --                 END
+    --             ) 
+    --             / COUNT(c.action), 
+    --         2
+    --     ), 
+    --     0
+    -- )
+    -- instead of count can use avg
