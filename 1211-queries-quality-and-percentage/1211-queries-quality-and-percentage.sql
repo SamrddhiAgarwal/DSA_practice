e5888ff18d481e9a636d2a5c98b00de99e546540
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-09-07 22:58:10 +0530

    Time: 450 ms (24.19%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -6,3 +6,14 @@ select query_name,
     round( count(case when rating <3 then 1 end)/count(*) * 100 ,2)  as poor_query_percentage
 from Queries
 group by query_name
+
+
+-- SELECT
+--     query_name,
+--     ROUND(AVG(rating / position), 2) AS quality
+-- FROM (
+--     SELECT DISTINCT query_name, rating, position
+--     FROM Queries
+-- ) q
+-- GROUP BY query_name;
+-- if was need to remove dupliacye rows
