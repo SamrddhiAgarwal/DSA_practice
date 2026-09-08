f8293e04fc7fa1bc5ddff5c18653221d100f4bc2
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-09-09 00:44:05 +0530

    Time: 583 ms (16.24%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,15 +1,22 @@
 # Write your MySQL query statement below
 -- 
-Select max(num) as num
-from (select num 
-from MyNumbers
-group by num
-having count(*)=1)t
+-- Select max(num) as num
+-- from (select num 
+-- from MyNumbers
+-- group by num
+-- having count(*)=1)t
 
 
--- SELECT
--- (SELECT num 
+SELECT
+(SELECT num 
+From MyNumbers
+Group by num
+Having COUNT(num)=1
+Order by num desc LIMIT 1) As num
+
+-- SELECT ifnull(num,null) as num
 -- From MyNumbers
 -- Group by num
 -- Having COUNT(num)=1
--- Order by num desc LIMIT 1) As num
+-- Order by num desc LIMIT 1
+-- need an outer query for this 
