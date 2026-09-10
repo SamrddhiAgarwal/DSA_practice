f94d94918a42312ff68d060906daced1b83d8b8e
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-09-11 01:19:28 +0530

    Time: 508 ms (47.52%), Space: 0B (100%) - LeetHub

---
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select sell_date, count(distinct product) as num_sold, group_concat(distinct product order by product ) as products
+From Activities group by sell_date order by sell_date
+--  group_concat(distinct product order by product ) as products order by is use because lexicology is written
