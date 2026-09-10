a2e90d75735b22997aefecf176a971e90ed13998
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-09-10 21:38:46 +0530

    Time: 0 ms (100%), Space: 79.7 MB (99.04%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,20 +1,48 @@
+// class Solution {
+// public:
+//     int totalFruit(vector<int>& fruits) {
+//         unordered_map<int,int>mp;
+//         int l=0,ans=0,n=fruits.size();
+//         for(int r=0;r<n;r++)
+//         {
+//             mp[fruits[r]]++;
+//             if(mp.size()>2)
+//             {
+//                 mp[fruits[l]]--;
+//                 if(mp[fruits[l]]==0)
+//                 mp.erase(fruits[l]);
+//                 l++;
+//             }
+//             if(mp.size()<=2)
+//             ans=max(ans,r-l+1);
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int,int>mp;
-        int l=0,ans=0,n=fruits.size();
-        for(int r=0;r<n;r++)
+        int lastfruit=-1,secondlastfruit=-1;
+        int currcnt=0,lastfruit_contcnt=0;
+        int ans=0;
+        for(auto & f:fruits)
         {
-            mp[fruits[r]]++;
-            if(mp.size()>2)
+            if(f==lastfruit || f==secondlastfruit)
+            currcnt++;
+
+            else
+            currcnt=lastfruit_contcnt+1;
+
+            if(f==lastfruit)
+            lastfruit_contcnt++;
+
+            else
             {
-                mp[fruits[l]]--;
-                if(mp[fruits[l]]==0)
-                mp.erase(fruits[l]);
-                l++;
+                lastfruit_contcnt=1;
+                secondlastfruit=lastfruit;
+                lastfruit=f;
             }
-            if(mp.size()<=2)
-            ans=max(ans,r-l+1);
+            ans=max(ans,currcnt);
         }
         return ans;
     }
