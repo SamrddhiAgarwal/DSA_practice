519b0114f3c2c3c257750a19e7a6da8f8a45acb7
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-09-12 00:31:49 +0530

    Time: 0 ms (100%), Space: 97.9 MB (99.03%) - LeetHub

--- 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
@@ -1,25 +1,55 @@
+// class Solution {
+// public:
+//     int partitionDisjoint(vector<int>& nums) {
+        
+//         // find min element
+//         int mn=min_element(nums.begin(),nums.end())-nums.begin();
+//         int mx=nums[0];
+//         int n=nums.size();
+//         for(int i=0;i<=mn;i++)
+//         {
+//             mx=max(mx,nums[i]);
+//         }
+//         int l=n,nmx=mx;
+//         for(int i=mn+1;i<n;i++)
+//         {
+//             if(nums[i]>=mx)
+//             {l=min(l,i);
+//             nmx=max(nmx,nums[i]);}
+//             if(nums[i]<mx)
+//             {
+//                 mx=nmx;
+//                 l=max(l,i+1);
+//             }
+            
+//         }
+//         return l;
+//     }
+// };
 class Solution {
 public:
     int partitionDisjoint(vector<int>& nums) {
         
         // find min element
-        int mn=min_element(nums.begin(),nums.end())-nums.begin();
+        // int mn=min_element(nums.begin(),nums.end())-nums.begin();
         int mx=nums[0];
         int n=nums.size();
-        for(int i=0;i<=mn;i++)
+        // for(int i=0;i<=mn;i++)
+        // {
+        //     mx=max(mx,nums[i]);
+        // }
+        int l=1,nmx=mx;
+        for(int i=1;i<n;i++)
         {
-            mx=max(mx,nums[i]);
-        }
-        int l=n,nmx=mx;
-        for(int i=mn+1;i<n;i++)
-        {
-            if(nums[i]>=mx)
-            {l=min(l,i);
-            nmx=max(nmx,nums[i]);}
+            // if(nums[i]>=mx)
+            // {l=min(l,i);
+            nmx=max(nmx,nums[i]);
+            // }
             if(nums[i]<mx)
             {
                 mx=nmx;
-                l=max(l,i+1);
+                // l=max(l,i+1);
+                l=i+1;
             }
             
         }
