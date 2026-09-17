680c6846fb081ddb28331a2351d887037c93fbc9
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-09-18 00:47:54 +0530

    Time: 48 ms (41.37%), Space: 76.2 MB (29.01%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj,vector<int>& color,int v)
-    {
-        for(auto u:adj[v])
-        {
-            if(color[u]==0)
-            {
-                color[u]=-color[v];
-                if(dfs(adj,color,u)==false) return false;
-            }
-            else
-            {
-                if(color[u]==color[v])
-                return false;
-            }
-        }
-        return true;
-    }
+    // bool dfs(vector<vector<int>>& adj,vector<int>& color,int v)
+    // {
+    //     for(auto u:adj[v])
+    //     {
+    //         if(color[u]==0)
+    //         {
+    //             color[u]=-color[v];
+    //             if(dfs(adj,color,u)==false) return false;
+    //         }
+    //         else
+    //         {
+    //             if(color[u]==color[v])
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         
         vector<vector<int>> graph(n+1);
@@ -30,19 +30,19 @@ public:
 
         vector<int> color(n+1,0);
 
-        for(int i=1;i<=n;i++)
-        {
-            if(color[i]==0)
-            {
-                color[i]=1;
-                if(dfs(graph,color,i)==false)
-                return false;
-            }
-        }
-        return true;
+        // for(int i=1;i<=n;i++)
+        // {
+        //     if(color[i]==0)
+        //     {
+        //         color[i]=1;
+        //         if(dfs(graph,color,i)==false)
+        //         return false;
+        //     }
+        // }
+        // return true;
 
 
-        /*queue<int>q;
+        queue<int>q;
 
         for(int i=1;i<=n;i++)
         {
@@ -72,6 +72,6 @@ public:
             
         }
         return true;
-        */
+        
     }
 };
