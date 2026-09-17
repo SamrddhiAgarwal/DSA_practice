2f44dbcc083bb7cbc032675758b68257581e850b
SamrddhiAgarwal <samrddhitia.9704@gmail.com>
2026-09-18 00:45:03 +0530

    Time: 43 ms (82.78%), Space: 29 MB (92.48%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,58 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        set<vector<int>> st;
-        vector<int> temp;
-        int k=0;
-       for(int i=0;i<nums.size();i++)
-        {
-            int target=k-nums[i];
-            for(int j=0;j<i;j++)
-            {
-                int min=target-nums[j];
-                if(mp.find(min)!=mp.end() && mp[min]!=j)
-                {
-                    temp={nums[i],nums[j],min};
-                    sort(temp.begin(),temp.end());
-                    st.insert(temp);
-                }
+        std::vector<std::vector<int>> result;
+        int n = nums.size();
+        
+        // Step 1: Sort the array to use the two-pointer technique
+        std::sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < n - 2; ++i) {
+            // Early Stopping: If the smallest number is > 0, 
+            // no three numbers can sum to 0
+            if (nums[i] > 0) {
+                break;
             }
             
-           mp[nums[i]]=i;
-
+            // Skip duplicate values for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            // Step 2: Initialize two pointers
+            int left = i + 1;
+            int right = n - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    
+                    // Skip duplicate values for the left pointer
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    // Skip duplicate values for the right pointer
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    
+                    // Move both pointers inward after finding a valid triplet
+                    left++;
+                    right--;
+                } 
+                else if (sum < 0) {
+                    // Sum is too small, move left pointer to get a larger number
+                    left++;
+                } 
+                else {
+                    // Sum is too large, move right pointer to get a smaller number
+                    right--;
+                }
+            }
         }
-        vector<vector<int>> ans(st.begin(),st.end());
-
-        return ans;
-      
+        
+        return result;
     }
 };
